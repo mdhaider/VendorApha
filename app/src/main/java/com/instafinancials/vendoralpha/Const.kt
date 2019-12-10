d0e6f0915c9a8c4413d7e93e14c97f6009312874
Nehal <nehal.hdr@gmail.com>
2019-12-11 02:34:59 +0530

    Tab view implemented

---
+++ app/src/main/java/com/instafinancials/vendoralpha/Const.kt
@@ -0,0 +1,8 @@
+package com.instafinancials.vendoralpha
+
+object Const {
+    const val APP_VERSION = "APP_VERSION"
+    const val APP_VERSION_NAME = "APP_VERSION_NAME"
+    const val PREF_NAME = "vendorPref"
+    const val PRIVATE_MODE = 0
+}
--- app/src/main/java/com/instafinancials/vendoralpha/ui/home/HomeFragment.kt
+++ app/src/main/java/com/instafinancials/vendoralpha/ui/home/HomeFragment.kt
@@ -1,18 +1,27 @@
 package com.instafinancials.vendoralpha.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.instafinancials.vendoralpha.R
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.tabs.TabLayout
+import com.instafinancials.vendoralpha.*
+import com.instafinancials.vendoralpha.ui.CameraActivity
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var viewPager: ViewPager
+    private lateinit var tabs: TabLayout
+    private lateinit var searchView: SearchView
+    private lateinit var fab:FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +31,50 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
+        viewPager = root.findViewById(R.id.view_pager)
+        tabs = root.findViewById(R.id.tabs)
+        searchView = root.findViewById(R.id.searchView)
+        fab= root.findViewById(R.id.fab)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(s: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(s: String): Boolean {
+                if (s.length >= 15) {
+                    Log.d("HomeFragment", "Text character is more than 15")
+                }
+                return false
+            }
         })
+
+        fab.setOnClickListener {
+            val intent = Intent(activity, CameraActivity::class.java)
+           // intent.putExtra("contentUid", contentUid)
+            //intent.putExtra("imageUri", imageUri)
+            //intent.putExtra("destinationUid", userUid)
+            startActivity(intent)
+        }
+
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter =
+            SectionsPagerAdapter(
+                activity!!,
+                childFragmentManager
+            )
+        adapter.addFragment(BasicFragment(), "Basic")
+        adapter.addFragment(DetailFragment(), "Detail")
+        adapter.addFragment(LastFragment(), "Last")
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+
+        searchView.setQuery(AppPreferences.gstNum, false)
+
+    }
+
 }
