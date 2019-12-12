35a6e63c8a4b68ce1d5ea6019f047ca55724c290
Nehal <nehal.hdr@gmail.com>
2019-12-12 16:27:51 +0530

    *app icon added

--- app/src/main/java/com/instafinancials/vendoralpha/apicall/network/NewsApiService.kt
+++ app/src/main/java/com/instafinancials/vendoralpha/apicall/network/NewsApiService.kt
@@ -5,16 +5,17 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 
 object NewsApiService {
     //creating a Network Interceptor to add api_key in all the request as authInterceptor
     private val interceptor = Interceptor { chain ->
-        val url = chain.request().url().newBuilder().addQueryParameter("apiKey", API_KEY).build()
+        val url = chain.request().url.newBuilder().addQueryParameter("apiKey", API_KEY).build()
         val request = chain.request()
-                .newBuilder()
-                .url(url)
-                .build()
+            .newBuilder()
+            .url(url)
+            .build()
         chain.proceed(request)
     }
     // we are creating a networking client using OkHttp and add our authInterceptor.
@@ -22,10 +23,10 @@ object NewsApiService {
 
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder().client(apiClient)
-                .baseUrl("https://newsapi.org/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(CoroutineCallAdapterFactory())
-                .build()
+            .baseUrl("https://newsapi.org/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
     }
 
     val newsApi: NewsApiInterface = getRetrofit().create(NewsApiInterface::class.java)
