33bf16e1e5976451831b28c61eee65eb5a12088f
sud-tes <Goblet@123>
2019-12-14 19:47:00 +0530

    login screen design

--- app/src/main/java/com/instafinancials/vendoralpha/ui/login/LoginActivity.kt
+++ app/src/main/java/com/instafinancials/vendoralpha/ui/login/LoginActivity.kt
@@ -4,37 +4,20 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.instafinancials.vendoralpha.R
-import com.instafinancials.vendoralpha.databinding.ActivityLoginBinding
 import com.instafinancials.vendoralpha.hideKeyboard
 import com.instafinancials.vendoralpha.onChange
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+ //   private lateinit var binding: ActivityLoginNewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        setContentView(R.layout.activity_login_new)
+      //  binding = DataBindingUtil.setContentView(this, R.layout.activity_login_new)
 
-        binding.mNumber.onChange {
-            if (it.length == 10) {
 
-            }
-        }
-
-        binding.mPass.onChange {
-            if (it.length > 5) {
-                binding.btnReq.isEnabled = true
-                binding.btnReq.setTextColor(resources.getColor(R.color.white))
-            } else {
-                binding.btnReq.isEnabled = false
-            }
-        }
-
-        binding.btnReq.setOnClickListener {
-            hideKeyboard()
-           // showProgress(true)
-        }
 
     }
 }
