69889f77ee7c492363572ceadf1740bfce802112
Nehal <nehal.hdr@gmail.com>
2019-12-17 21:17:00 +0530

    *login ui changes

--- app/src/main/java/com/instafinancials/vendoralpha/ui/login/LoginActivity.kt
+++ app/src/main/java/com/instafinancials/vendoralpha/ui/login/LoginActivity.kt
@@ -1,23 +1,44 @@
 package com.instafinancials.vendoralpha.ui.login
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.instafinancials.vendoralpha.R
+import com.instafinancials.vendoralpha.databinding.ActivityLoginBinding
 import com.instafinancials.vendoralpha.hideKeyboard
 import com.instafinancials.vendoralpha.onChange
-import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
- //   private lateinit var binding: ActivityLoginNewBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_new)
-      //  binding = DataBindingUtil.setContentView(this, R.layout.activity_login_new)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
+        binding.mNumber.onChange {
+            if (it.length == 10) {
+                binding.otpParent.visibility = View.VISIBLE
+            }
+        }
 
+        binding.mOtp.onChange {
+            if (it.length ==4) {
+                binding.btnReq.visibility=View.VISIBLE
+                binding.btnReq.isEnabled = true
+                binding.btnReq.setTextColor(resources.getColor(R.color.white))
+            } else {
+                binding.btnReq.isEnabled = false
+                binding.btnReq.visibility=View.VISIBLE
+                binding.btnReq.setTextColor(resources.getColor(R.color.grey_500))
+            }
+        }
+
+        binding.btnReq.setOnClickListener {
+            hideKeyboard()
+            // showProgress(true)
+        }
 
     }
 }
