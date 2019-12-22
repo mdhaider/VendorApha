ce487d93d595489fc7279c9a466c504ea845338a
Nehal <nehal.hdr@gmail.com>
2019-12-22 18:46:39 +0530

    new api changes

--- app/src/main/java/com/instafinancials/vendoralpha/shared/ApiConstants.kt
+++ app/src/main/java/com/instafinancials/vendoralpha/shared/ApiConstants.kt
@@ -4,7 +4,7 @@ object ApiConstants {
     const val BASE_URL = "https://apps.instafinancials.com/"
     const val REQUEST_TIMEOUT = 30
     const val RETROFIT_LOG="Retrofit"
-    const val ACCES_TOKEN_KEY= "AccessTocken"
+    const val ACCES_TOKEN_KEY= "AccessToken"
     const val GST_DATA_ENDPOINT= "InstaGST/v1/json/GSTIN/{GSTIN}"
 
 }
